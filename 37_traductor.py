bd9a973c17a0e56bd4116dce9ebdaaaf50db2a1e
adolfo-martin <adolfo.martin@iesramonarcas.es>
2023-11-14 12:22:52 +0100

    hoy

---
+++ 37_traductor.py
@@ -0,0 +1,16 @@
+import os
+os.system('cls')
+
+colores_espaniol = ["Rojo", "Verde", "Azul", "Amarillo", "Negro", "Naranja", "Blanco", "Marrón"]
+colores_ingles = ["Red", "Green", "Blue", "Yellow", "Black", "Orange", "White", "Brown"]
+colores_frances = ["Rouge", "Vert", "Bleu", "Jaune", "Noir", "Orange", "Blanc", "Brun"]
+colores_aleman = ["Rot", "Grün", "Blau", "Gelb", "Schwarz", "Orange", "Weiß", "Braun"]
+
+def traducir_espaniol_ingles(palabra):
+    for i in range(0, len(colores_espaniol)):
+        if colores_espaniol[i] == palabra:
+            return colores_ingles[i]
+
+
+
+print(traducir_espaniol_ingles('Negro'))
